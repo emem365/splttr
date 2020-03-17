95dcda38a6f3e2dc9751ba9ea2d6b46d41f7b8b2
Madhur Maurya <madhurmaurya365@gmail.com>
2020-03-17 13:17:33 +0530

    Adding a Bottom Nav Bar. Needs theming

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,71 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'splttr',
-      home: Material(
-        child: Center(
-          child: Text(
-            'SPLTTR - Splash',
-            style: TextStyle(
-              fontSize: 80,
-              fontWeight: FontWeight.w100,
-              letterSpacing: 10.0,
-            ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState(){
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.yellow,
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('Drawer Header'),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+              ),
+            ],
           ),
+        ),
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('SPLTTR'),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.search), onPressed: null),
+          ],
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Center(child: Text('Tab1')),
+            Center(child: Text('Tab2')),
+            Center(child: Text('Tab3')),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: null,
+          label: Text('Add'),
+          icon: Icon(Icons.add),
+        ),
+        bottomNavigationBar: TabBar(
+          labelColor: Colors.red,
+          tabs: <Widget>[
+            Tab(
+              icon: Icon(Icons.search),
+            ),
+            Tab(
+              icon: Icon(Icons.search),
+            ),
+            Tab(
+              icon: Icon(Icons.search),
+            ),
+          ],
         ),
       ),
     );
