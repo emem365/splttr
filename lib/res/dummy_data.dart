f4fc97c5bfc224f6a40aead3f7a5ba8402138749
Madhur Maurya <madhurmaurya365@gmail.com>
2020-04-22 22:15:00 +0530

    Outings page UI

--- lib/res/dummy_data.dart
+++ lib/res/dummy_data.dart
@@ -1,3 +1,4 @@
+
 class DummyData{
   static const usersOweYouList = [
     {
@@ -55,6 +56,68 @@ class DummyData{
       'last_trans': '10-03-2020',
       'amount': -28,
       'avatar' : 'yoga',
+    },
+  ];
+  static final outingsList = [
+    {
+      'outing-name' : 'Goa Trip',
+      'friends' : ['Akhilesh', 'Dharma', 'Jayesh', 'Mickey'],
+      'date': DateTime(2022, 8, 14),
+      'avatar' : 'travel-bag',
+    },
+    {
+      'outing-name' : 'New Year Celebration',
+      'friends' : ['Akhilesh', 'Jayesh', 'Ayush', 'Tarun'],
+      'date': DateTime(2021, 12, 31),
+      'avatar' : 'living-room',
+    },
+    {
+      'outing-name' : 'Blah blah',
+      'friends' : ['blah', 'badi', 'bu', 'blah'],
+      'date': DateTime(2021, 8, 9),
+      'avatar' : 'school-bus',
+    },
+    {
+      'outing-name' : 'Blah blah 2',
+      'friends' : ['blah', 'bu', 'blah'],
+      'date': DateTime(2021, 9, 14),
+      'avatar' : 'sale',
+    },
+    {
+      'outing-name' : 'Blah blah 3',
+      'friends' : ['bu', 'blah'],
+      'date': DateTime(2021, 11, 2),
+      'avatar' : 'wallet',
+    },
+    {
+      'outing-name' : 'Blah blah 4',
+      'friends' : ['blah', 'badi', 'bu', 'blah'],
+      'date': DateTime(2021, 10, 11),
+      'avatar' : 'stove',
+    },
+    {
+      'outing-name' : 'Blah blah 5',
+      'friends' : ['badi', 'bu', 'blah'],
+      'date': DateTime(2021, 8, 9),
+      'avatar' : 'mario',
+    },
+    {
+      'outing-name' : 'Blah blah 6',
+      'friends' : ['blah', 'badi', 'bu', 'blah'],
+      'date': DateTime(2020, 8, 9),
+      'avatar' : 'golf-ball',
+    },
+    {
+      'outing-name' : 'Blah blah 7',
+      'friends' : ['blah',  'bu', 'blah'],
+      'date': DateTime(2021, 8, 9),
+      'avatar' : 'drink',
+    },
+    {
+      'outing-name' : 'Blah blah infinity',
+      'friends' : ['blah', 'badi', 'bu', 'blah'],
+      'date': DateTime(2020, 04, 22),
+      'avatar' : 'passport',
     },
   ];
 }
--- lib/res/resources.dart
+++ lib/res/resources.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:splttr/res/avatars.dart';
 import 'package:splttr/res/currency.dart';
-import 'package:splttr/res/colors.dart';
 
 class Tile extends StatelessWidget {
   final String avatar;
@@ -11,7 +10,7 @@ class Tile extends StatelessWidget {
   final int amount;
   final void Function() onTap;
   final Color splashColor;
-
+  final Color backgroundColor;
 
   const Tile({
     Key key,
@@ -19,6 +18,7 @@ class Tile extends StatelessWidget {
     this.title = '',
     this.body = '',
     this.subtitle = '',
+    this.backgroundColor,
     this.amount,
     this.onTap,
     this.splashColor,
@@ -26,77 +26,80 @@ class Tile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      splashColor: splashColor,
-      onTap: onTap,
-      child: Card(
-      color: PurpleTheme.lightPurple,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-        child: Row(
-          children: <Widget>[
-            CircleAvatar(
-              child: Avatars.getAssetFromName(avatar),
-              radius: MediaQuery.of(context).size.width / 8,
-            ),
-            SizedBox(
-              width: (amount == null)
-                  ? MediaQuery.of(context).size.width / 2
-                  : MediaQuery.of(context).size.width / 2.5,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(left: 8.0, bottom: 4.0),
-                    child: Text(
-                      title,
-                      style: Theme.of(context).textTheme.subhead.copyWith(
-                            fontSize: 16.0,
-                          ),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 8.0, bottom: 4.0),
-                    child: Text(
-                      body,
-                      style: Theme.of(context).textTheme.body1,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 8.0, bottom: 4.0),
-                    child: Text(
-                      subtitle,
-                      style: Theme.of(context).textTheme.subtitle.copyWith(
-                            color: Colors.black54,
-                          ),
-                    ),
-                  ),
-                ],
+    return Card(
+      color: backgroundColor,
+      child: InkWell(
+        splashColor: splashColor,
+        onTap: onTap,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+          child: Row(
+            children: <Widget>[
+              CircleAvatar(
+                child: Avatars.getAssetFromName(avatar),
+                radius: MediaQuery.of(context).size.width / 8,
               ),
-            ),
-            amount != null
-                ? Expanded(
-                    child: Text(
-                      '${amount > 0 ? '+' : ''}${Currency.currencyFormat.format(amount).replaceAll(".00", "")}',
-                      style: TextStyle(
-                        color: amount > 0
-                            ? Currency.profitColor
-                            : Currency.lossColor,
-                        fontSize: 16.0,
+              SizedBox(
+                width: (amount == null)
+                    ? MediaQuery.of(context).size.width / 2
+                    : MediaQuery.of(context).size.width / 2.5,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: (amount == null) ? 16.0 : 8.0, bottom: 4.0),
+                      child: Text(
+                        title,
+                        style: Theme.of(context).textTheme.subhead.copyWith(
+                              fontSize: 16.0,
+                            ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
                       ),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
                     ),
-                  )
-                : Container(),
-          ],
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: (amount == null) ? 16.0 : 8.0, bottom: 4.0),
+                      child: Text(
+                        body,
+                        style: Theme.of(context).textTheme.body1,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: (amount == null) ? 16.0 : 8.0, bottom: 4.0),
+                      child: Text(
+                        subtitle,
+                        style: Theme.of(context).textTheme.subtitle.copyWith(
+                              color: Colors.black54,
+                            ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              amount != null
+                  ? Expanded(
+                      child: Text(
+                        '${amount > 0 ? '+' : ''}${Currency.currencyFormat.format(amount).replaceAll(".00", "")}',
+                        style: TextStyle(
+                          color: amount > 0
+                              ? Currency.profitColor
+                              : Currency.lossColor,
+                          fontSize: 16.0,
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    )
+                  : Container(),
+            ],
+          ),
         ),
       ),
-    ),
     );
   }
 }
